5d91798fbe346e582dabe5ee88a8df05fefc9037
Lalit <lalit@SQMLaptop33.local>
2021-10-18 15:29:32 +0530

    Added Missing file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
+	const [userList, setUserList] = useState([]);
+
+	const addUserHandler = (userName, age) => {
+		setUserList((prevUserList) => {
+			return [...prevUserList, {"name":userName, "age": age, "id": Math.random().toString()}];
+		});
+	}
   return (
     <div>
-    	<AddUser />
-    	<UsersList users= {[]} />
+    	<AddUser onAddUser={addUserHandler} />
+    	<UsersList users= {userList} />
     </div>
   );
 }
