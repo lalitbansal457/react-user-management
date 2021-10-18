a0eac35f859dd3a0676498f9005f4a8c1a45b29c
Lalit <lalit@SQMLaptop33.local>
2021-10-18 15:44:51 +0530

    Added error model for showing the error

--- src/components/Users/addUser.js
+++ src/components/Users/addUser.js
@@ -2,16 +2,22 @@ import {useState} from "react";
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 import classes from "./AddUser.module.css";
 
 const AddUser = props => {
 
 	const [enteredUsername, setEnteredUsername] = useState("")
 	const [enteredAge, setEnteredAge] = useState("")
+	const [error, setError] = useState()
 
 	const addUserHandler = (event) => {
 		event.preventDefault();
 		if(!enteredUsername.trim().length || !enteredAge.trim().length) {
+			setError({
+				"title": "Invalid input",
+				"message": "Please enter valid value"
+			})
 			return;
 		}
 		props.onAddUser(enteredUsername, enteredAge);
@@ -28,18 +34,28 @@ const AddUser = props => {
 		setEnteredAge(event.target.value);
 	}
 
+	const errorHandler = () => {
+		setError(null);
+	}
+
+
+
 	return (
-		<Card className={classes.input}>
-			<form onSubmit={addUserHandler}>
-				<label htmlFor="username">Username</label>
-				<input type="text" id="username" value={enteredUsername} onChange={usernameHandler} / >
+		<div>
+			{error && <ErrorModal title={error.title} onConfirm={errorHandler} message={error.message} />}
+			<Card className={classes.input}>
+				<form onSubmit={addUserHandler}>
+					<label htmlFor="username">Username</label>
+					<input type="text" id="username" value={enteredUsername} onChange={usernameHandler} / >
 
-				<label htmlFor="username">Age (Years)</label>
-				<input type="number" id="age" value={enteredAge} onChange={ageHandler} / >	
+					<label htmlFor="username">Age (Years)</label>
+					<input type="number" id="age" value={enteredAge} onChange={ageHandler} / >	
 
-				<Button type="submit">Add User</Button>
-			</form>
-		</Card>
+					<Button type="submit">Add User</Button>
+				</form>
+			</Card>
+		</div>
+		
 	)
 }
 
